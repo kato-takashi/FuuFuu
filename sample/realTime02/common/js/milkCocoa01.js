6445295cb659d74f057935e52f0ce90210f9b0e0
TK_Ratina <taka4.kato@gmail.com>
2015-06-02 09:57:05 +0900

    配列に格納

--- sample/realTime02/common/js/milkCocoa01.js
+++ sample/realTime02/common/js/milkCocoa01.js
@@ -5,6 +5,13 @@ $(function() {
         //2."message"データストアを作成
         var ds = milkcocoa.dataStore("message");
 
+        var windPower = document.getElementById('windPower');
+        var windInterval;
+        var windPowerArr = [];
+        var chatTitle = "chat"
+        var chatText = $("#content").val();
+        var iteraterNum = 0;
+
         //3."message"データストアからメッセージを取ってくる
         ds.stream().sort("desc").next(function(err, datas) {
             datas.forEach(function(data) {
@@ -22,6 +29,7 @@ $(function() {
 
         var last_message = "dummy";
 
+        //データの読み込み
         function renderMessage(message) {
             var message_html = '<p class="post-text">' + escapeHTML(message.content) + '</p>';
             var date_html = '';
@@ -32,12 +40,13 @@ $(function() {
             last_message = message.id;
         }
 
-        function post() {
+        function post(titleStr, contentStr) {
+            var titleStr = titleStr || 'タイトルなし'
             //5."message"データストアにメッセージをプッシュする
-            var content = escapeHTML($("#content").val());
+            var content = escapeHTML(contentStr);
             if (content && content !== "") {
                 ds.push({
-                    title: "タイトル",
+                    title: titleStr,
                     content: content,
                     date: new Date().getTime()
                 }, function (e) {});
@@ -46,15 +55,48 @@ $(function() {
         }
 
         $('#post').click(function () {
-            post();
+            post(chatTitle, chatText);
         })
         $('#content').keydown(function (e) {
             if (e.which == 13){
-                post();
+                post(chatTitle, chatText);
                 return false;
             }
         });
+        /////////風の値のダミー
+        function randNum(){
+          // var randNum = Math.floor( Math.random() * 100 );
+          // console.log(randNum);
+          // windPower.innerHTML = randNum;
+          //   post('風の強さ', randNum);
+          iteraterNum += 1;
+          windPower.innerHTML = iteraterNum;
+          windPowerArr.push(iteraterNum);
+          //iteraterが10の倍数の時　post
+          if(iteraterNum%10 == 0){
+            post('風の強さ', windPowerArr);
+            console.log('post' + iteraterNum);
+            windPowerArr = [];  
+          }
+          
+        }
 
+        $('#startWind').click(function () {
+            startWind();
+        })
+
+        $('#stopWind').click(function () {
+            stopWind();
+        })
+
+        function startWind(){
+          windInterval = setInterval(randNum,1000);
+        }
+
+        function stopWind(){
+          clearInterval(windInterval);
+          console.log('stop');
+        }
         
     });
     //インジェクション対策

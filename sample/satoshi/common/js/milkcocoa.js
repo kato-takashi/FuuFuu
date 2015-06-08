37ab302c2a0cc81faeff8ae237e4bc9227f5329a
TK_Ratina <taka4.kato@gmail.com>
2015-06-08 09:08:50 +0900

    配列に格納してnaitiveに出力の関数化

--- sample/satoshi/common/js/milkcocoa.js
+++ sample/satoshi/common/js/milkcocoa.js
@@ -29,22 +29,15 @@ $(function() {
           //milkcocoaからデータを取得->配列を個々のstringに変換し、1つづつ出力
           function getData(){
             datas.forEach(function(data) {
-                // console.log(data.value);
+                console.log(data.value);
                 // console.log(data.value.title);
                 dataArray.push(data.value);
                 // console.log(data);
-                // console.log(data.id+ ": " + data.value.content);
-               
+                // console.log(data.id+ ": " + data.value.content);     
                });
-             // 配列に格納
-                var id = 49;//test id:49を取得
-                var resArray = dataArray[id].content.split(",");
-                // console.log(resArray);
-                //配列を一つづつ出力
-                for(var i = 0; i < resArray.length; i++){
-                  // console.log("resArray"+ resArray[i]);
-                  Native.showToast(resArray[i]);
-                }
+                var id = 316;//test id:316を取得
+                // 配列に格納してデータを一づつ出力　->Naitive
+                outNative(dataArray[id]);
             }
 
               $("#outPutBtn").click(function() {
@@ -55,8 +48,9 @@ $(function() {
                   getData();
                   return false;
               }
-          });        
-        
+          });
+
+                      
           //ネイティブからmilkcocoaに送信
           function addTextNode(windPower) {
               $(".nTOw").text(windPower);
@@ -94,12 +88,15 @@ $(function() {
 
           //すべてを表示ボタンのクリック
           $("#showAllPutBtn").click(function() {
-                console.log("showAllPutBtn");
+                // console.log("showAllPutBtn");
+                var message = { id: '1' , name: '加藤貴司' };;
+                renderMessage(message);
           }); 
 
           //非表示ボタンのクリック
           $("#hidePutBtn").click(function() {
-                console.log("hidePutBtn");
+                // console.log("hidePutBtn");
+                resetHTML();
           }); 
 
           //milkcocoaデータストアにプッシュ　引数1タイトル、引数2コンテンツ（風量）
@@ -118,5 +115,52 @@ $(function() {
                   }, function (e) {});
               }
           }
+          
+        //html表示メッセージの最後の行
+        var last_message = "dummy";
+
+        //HTMLにデータを表示
+        function renderMessage(message) {
+            // var message_html = '<p class="post-text">' + escapeHTML(message.content) + '</p>';
+            // var date_html = '';
+            // var button_html = '<button class="windOutPutBtn" id = "windId' + message.id +'">OutPut</button>';
+            // if(message.date) {
+            //     date_html = '<p class="post-date">'+'id: ' +escapeHTML(message.id) + ' ' + escapeHTML(message.title)+' : '+escapeHTML( new Date(message.date).toLocaleString())+'</p>';
+            // }
+            // $("#"+last_message).before('<div id="'+message.id+'" class="post">'+ button_html + message_html + date_html +'</div>');
+            $("#"+last_message).before('<div id="'+message.id+'" class="post">'+ escapeHTML(message.name) +'</div>');
+            // last_message = message.id;
+            
+            // /////出力
+            // $('#windId' + message.id).click(function () {
+            //     alert ('出力；'+ escapeHTML(message.content) );
+            // })
+        }                
+
+        //.content内のhtmlを消去
+        function resetHTML(){
+            $(".post").remove();
+            //html表示メッセージの最後の行を初期化
+            last_message = "dummy";
+            console.log('reset');
+        }
+
+          //////////今西さんのデータ
           addTextNode('CLOSE');
   });
+
+//インジェクション対策
+function escapeHTML(val) {
+    return $('<div>').text(val).html();
+};
+
+//配列を一つづつ取り出しネイティブに出力
+function outNative(arrayData){
+  var resArray = arrayData.content.split(",");
+  // console.log(resArray);
+  //配列を一つづつ出力
+  for(var i = 0; i < resArray.length; i++){
+    // console.log("resArray"+ resArray[i]);
+    Native.showToast(resArray[i]);
+  }
+}  

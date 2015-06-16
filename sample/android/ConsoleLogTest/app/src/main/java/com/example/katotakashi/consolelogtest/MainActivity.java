5bf7888c23af588f177b94d265dab8a8a480b295
TK_Ratina <taka4.kato@gmail.com>
2015-06-16 11:39:02 +0900

    jsのweb viewからconsole.logテスト

---
+++ sample/android/ConsoleLogTest/app/src/main/java/com/example/katotakashi/consolelogtest/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.katotakashi.consolelogtest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.ConsoleMessage;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+            WebView oWebView = new WebView(getApplicationContext());
+            oWebView.loadUrl("file:///android_asset/test.html");
+
+            oWebView.setWebChromeClient(new WebChromeClient() {
+                public boolean onConsoleMessage(ConsoleMessage cm) {
+                    Log.d("oWebView", cm.message() + " -- From line "
+                            + cm.lineNumber() + " of "
+                            + cm.sourceId());
+                    return true;
+                }
+            });
+        oWebView.setWebChromeClient(new WebChromeClient());
+
+        setContentView(oWebView);
+        oWebView.getSettings().setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

6f2a3d3002e81422f9e51404e5049870cc4c1943
TK_Ratina <taka4.kato@gmail.com>
2015-06-18 08:50:51 +0900

    カメラ撮影サンプル

---
+++ sample/android/CameraPreview04/app/src/main/java/com/example/katotakashi/camerapreview04/MainActivity.java
@@ -0,0 +1,146 @@
+package com.example.katotakashi.camerapreview04;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.hardware.Camera;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends Activity {
+    // カメラインスタンス
+    private Camera mCam = null;
+
+    // カメラプレビュークラス
+    private CameraPreview mCamPreview = null;
+
+    // 画面タッチの2度押し禁止用フラグ
+    private boolean mIsTake = false;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // カメラインスタンスの取得
+        try {
+            mCam = Camera.open();
+        } catch (Exception e) {
+            // エラー
+            this.finish();
+        }
+
+        // FrameLayout に CameraView クラスを設定
+        FrameLayout preview = (FrameLayout)findViewById(R.id.cameraPreview);
+        mCamPreview = new CameraPreview(this, mCam);
+        preview.addView(mCamPreview);
+
+        // mCamPreview に タッチイベントを設定
+        mCamPreview.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (!mIsTake) {
+                        // 撮影中の2度押し禁止用フラグ
+                        mIsTake = true;
+                        // 画像取得
+                        mCam.takePicture(null, null, mPicJpgListener);
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // カメラ破棄インスタンスを解放
+        if (mCam != null) {
+            mCam.release();
+            mCam = null;
+        }
+    }
+
+
+    /**
+     * JPEG データ生成完了時のコールバック
+     */
+    private Camera.PictureCallback mPicJpgListener = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            if (data == null) {
+                return;
+            }
+
+            String saveDir = Environment.getExternalStorageDirectory().getPath() + "/test";
+
+            // SD カードフォルダを取得
+            File file = new File(saveDir);
+            Log.e("saveDir", saveDir);
+            // フォルダ作成
+            if (!file.exists()) {
+                if (!file.mkdir()) {
+                    Log.i("Debug", "Make Dir Error");
+                }
+            }
+
+            // 画像保存パス
+            Calendar cal = Calendar.getInstance();
+            SimpleDateFormat sf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+            String imgPath = saveDir + "/" + sf.format(cal.getTime()) + ".jpg";
+            Log.i("imgPath", imgPath);
+            // ファイル保存
+            FileOutputStream fos;
+            try {
+                fos = new FileOutputStream(imgPath, true);
+                fos.write(data);
+                fos.close();
+                Log.i("imgPath", imgPath);
+                // アンドロイドのデータベースへ登録
+                // (登録しないとギャラリーなどにすぐに反映されないため)
+                registAndroidDB(imgPath);
+                Toast.makeText(getApplicationContext(), "写真が保存されました。", Toast.LENGTH_SHORT).show();
+
+            } catch (Exception e) {
+                Log.e("Debug", e.getMessage());
+                Toast.makeText(getApplicationContext(), "保存時にエラーが発生しました。", Toast.LENGTH_SHORT).show();
+            }
+
+            fos = null;
+
+            // takePicture するとプレビューが停止するので、再度プレビュースタート
+            mCam.startPreview();
+
+            mIsTake = false;
+        }
+    };
+
+    /**
+     * アンドロイドのデータベースへ画像のパスを登録
+     * @param path 登録するパス
+     */
+    private void registAndroidDB(String path) {
+        // アンドロイドのデータベースへ登録
+        // (登録しないとギャラリーなどにすぐに反映されないため)
+        ContentValues values = new ContentValues();
+        ContentResolver contentResolver = MainActivity.this.getContentResolver();
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        values.put("_data", path);
+        contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+    }
+}
